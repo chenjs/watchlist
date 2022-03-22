21db97e594f73fd3247b49eca4a47bd9f703f714
chenjs <2363759499@qq.com>
2022-03-22 13:54:52 +0800

    Add index page

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 
 app = Flask(__name__)
 
+name = "Tom Chen"
+movies = [
+	{'title' : 'My Neighbor Totoro', 'year':1988},
+	{'title': 'Dead Poets Society', 'year': '1989'},
+    {'title': 'A Perfect World', 'year': '1993'},
+    {'title': 'Leon', 'year': '1994'},
+    {'title': 'Mahjong', 'year': '1996'},
+    {'title': 'Swallowtail Butterfly', 'year': '1996'},
+    {'title': 'King of Comedy', 'year': '1999'},
+    {'title': 'Devils on the Doorstep', 'year': '1999'},
+    {'title': 'WALL-E', 'year': '2008'},
+    {'title': 'The Pork of Music', 'year': '2012'}
+]
 
 @app.route('/')
+def index():
+	return render_template('index.html', name=name, movies=movies)
+	
+
 @app.route('/home')
-def hello():
+def home():
 # 	return 'Welcome to My Watchlist Demo'
 # 	return u'欢迎来到WatchList的世界！'
 	return '<h1>Hello Totoro!</h1><img src="http://helloflask.com/totoro.gif">'
@@ -28,5 +45,7 @@ def test_url_for():
 	
 	return 'test_page'
 	
+	
+
 	
 
