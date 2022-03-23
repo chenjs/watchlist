77e42289e7d622321ae435f84a56f5afd5d209e2
chenjs <2363759499@qq.com>
2022-03-23 22:24:47 +0800

    Create, edit and delete item by form

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
+from crypt import methods
 from flask import Flask, session, url_for, render_template
+from flask import request, redirect, flash
 from flask_sqlalchemy import SQLAlchemy
 import os
 import sys
@@ -8,6 +10,7 @@ app = Flask(__name__)
 
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////" + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'fdajfsdjfa'
 
 db = SQLAlchemy(app)
 
@@ -59,12 +62,47 @@ class Movie(db.Model):
 	year = db.Column(db.String(4))
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-	# return render_template('index.html', name=name, movies=movies)
+	if request.method == "POST":
+		title = request.form.get('title')
+		year = request.form.get('year')
+		if not title or not year or len(year)<4 or len(title)>60:
+			flash("Invalid input.")
+			return redirect(url_for('index'))
+		
+		movie = Movie(title=title, year=year)
+		db.session.add(movie)
+		db.session.commit()
+		flash("Item created.")
+		return redirect(url_for('index'))
+
 	movies = Movie.query.all()
 	return render_template('index.html', movies=movies)
 	
+@app.route('/movie/edit/<int:movie_id>', methods=['GET', 'POST'])
+def edit(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	if request.method == 'POST':
+		title = request.form['title']
+		year = request.form['year']
+		if not title or not year or len(year)<4 or len(title)>60:
+			flash("Invalid input.")
+			return redirect(url_for('edit'), movie_id=movie_id)
+		movie.title = title
+		movie.year = year
+		db.session.commit()
+		flash('Item updated.')
+		return redirect(url_for('index'))
+	return render_template('edit.html', movie=movie)
+
+@app.route('/movie/delete/<int:movie_id>', methods=['POST'])
+def delete(movie_id):
+	movie = Movie.query.get_or_404(movie_id)
+	db.session.delete(movie)
+	db.session.commit()
+	flash('Item deleted.')
+	return redirect(url_for('index'))
 
 @app.route('/home')
 def home():
